44006eef08cc6f57396e0e1c2a06417e4e2e0992
Julio Rincon <jcrinconc@gmail.com>
2015-06-16 17:18:34 +1000

    fixes

--- resources/public/js/common-widgets.js
+++ resources/public/js/common-widgets.js
@@ -1,4 +1,19 @@
 angular.module('common-widgets', [])
+    .factory('focus', function ($rootScope, $timeout) {
+        return function(name) {
+            $timeout(function (){
+                $rootScope.$broadcast('focusOn', name);
+            });
+        }
+    }).directive('focusOn', function() {
+        return function(scope, elem, attr) {
+            scope.$on('focusOn', function(e, name) {
+                if(name === attr.focusOn) {
+                    elem[0].focus();
+                }
+            });
+        };
+    })
     .directive('loginForm', function($log){
         return {
             scope:{
@@ -15,7 +30,7 @@ angular.module('common-widgets', [])
                     console.debug('param ', loginData, " login ", $scope.login);
                     $scope.clicked=$scope.loginFn(loginData);
                 };
-            }            
+            }
         };
     })
 ;
