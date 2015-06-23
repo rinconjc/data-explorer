304d36ba581d378c1fb446e889c68848fe34e41b
Julio <jcrinconc@gmail.com>
2015-06-24 07:44:57 +1000

    upgraded upgrader

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-ring "0.8.11"]]
   :ring {:handler dbquery.core/all-routes}
+  :repositories [["jitpack" {:url "https://jitpack.io" :snapshots false}]]
   :dependencies [[compojure "1.3.4"]
                  [ring/ring-json "0.3.1"]
                  [ring/ring-defaults "0.1.5"]
@@ -13,7 +14,7 @@
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [korma "0.4.1"]
-                 [com.rinconj/dbupgrader "1.0-SNAPSHOT"]
+                 [com.github.rinconjc/db-upgrader "1.0-beta7"]
                  [com.h2database/h2 "1.4.187"]
                  [org.clojure/java.jdbc "0.3.6"]
                  [crypto-password "0.1.3"]
