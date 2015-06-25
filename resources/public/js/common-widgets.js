d2f7ddf23859bb3549f8fd3540ca26badde5df91
Julio <jcrinconc@gmail.com>
2015-06-26 07:16:14 +1000

    sorting

--- resources/public/js/common-widgets.js
+++ resources/public/js/common-widgets.js
@@ -51,10 +51,24 @@ angular.module('common-widgets', [])
             },
             replace:true,
             transclude:true,
-            template:'<table class="table {{class}}" data-len="{{columns.length}}"><thead><tr><th ng-repeat="col in columns">{{col}} <sort-button field="{{col}}" sort-fn="sorter"/></th></tr></thead><tbody><tr ng-repeat="row in data"><td ng-repeat="item in row track by $index" title="{{item}}">{{item}}</td></tr></tbody></table>',
+            template:'<table class="table {{class}}" data-len="{{columns.length}}"><thead><tr><th ng-repeat="col in columns">{{col}} <sort-button field="{{col}}" sort-fn="sorter"/></th></tr></thead><tbody><tr ng-repeat="row in data | orderBy:sortState"><td ng-repeat="item in row track by $index" title="{{item}}">{{item}}</td></tr></tbody></table>',
             controller:function($scope){
+                $scope.sortState=[];
                 $scope.sorter = function(col, ascDesc){
                     console.debug('sorting by', col, ascDesc);
+                    if(ascDesc=='+'){
+                        $scope.sortState.push(col);
+                    } else if(ascDesc=='-'){
+                        var i=$scope.sortState.indexOf(col);
+                        if(i>=0){
+                            $scope.sortState[i] = '-'+col;
+                        }
+                    } else{
+                        var i=$scope.sortState.indexOf('-'+col);
+                        if(i>=0)
+                            $scope.sortState.splice(i,1);
+                    }
+                    console.debug('sort state:', $scope.sortState);
                 };
             }
         };
