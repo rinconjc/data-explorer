2c7387d364eb03f74428670e17a632a13fa47104
Julio <jcrinconc@gmail.com>
2015-06-26 07:16:14 +1000

    sorting

--- resources/public/js/data-table.js
+++ resources/public/js/data-table.js
@@ -8,10 +8,23 @@ angular.module('data-table',[])
             },
             replace:true,
             transclude:true,
-            template:'<table class="table {{class}}" data-len="{{columns.length}}"><thead><tr><th ng-repeat="col in columns">{{col}}<sort-button1 field="{{col}}" sortFn="sorter"/></th></tr></thead><tbody><tr ng-repeat="row in data"><td ng-repeat="item in row track by $index" title="{{item}}">{{item}}</td></tr></tbody></table>',
+            template:'<table class="table {{class}}" data-len="{{columns.length}}"><thead><tr><th ng-repeat="col in columns">{{col}}<sort-button1 field="{{col}}" sortFn="sorter"/></th></tr></thead><tbody><tr ng-repeat="row in data | orderBy:sortState"><td ng-repeat="item in row track by $index" title="{{item}}">{{item}}</td></tr></tbody></table>',
             controller:function($scope){
+                $scope.sortState=[];
                 $scope.sorter = function(col, ascDesc){
                     console.debug('sorting by', col, ascDesc);
+                    if(ascDesc=='+'){
+                        $scope.sortState.push('+'+col);
+                    } else if(ascDesc=='-'){
+                        var i=$scope.sortState.indexOf('+'+col);
+                        if(i>=0)
+                            $scope.sortState[i] = '-'+col;
+                    } else{
+                        var i=$scope.sortState.indexOf('-'+col);
+                        if(i>=0)
+                            $scope.sortState.splice(i,1);
+                    }
+                    console.debug('sort state', $scope.sortState);
                 };
             }
         };
