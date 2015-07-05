d1e32a6c1a510e781bfeb3d3131ce220c14f5d66
Julio <jcrinconc@gmail.com>
2015-07-06 07:46:25 +1000

    refactorings

--- resources/public/js/query-builder.js
+++ resources/public/js/query-builder.js
@@ -1,7 +1,22 @@
 angular.module('query.builder',[])
     .directive('queryBuilder', function(){
         return {
-            templateUrl:'tpls/query-builder.html'
+            scope:{
+                dsId:'='
+            },
+            templateUrl:'tpls/query-builder.html',
+            controller:function($scope, DataService){
+                $scope.model = {};
+                $scope.fromTables=[];
+                $scope.tables = DataService.getTables($scope.dsId);
+                $scope.addTable = function(item){
+                    $scope.fromTables.push(item);
+                    $scope.model.table = '';
+                };
+                $scope.delTable = function(index){
+                    $scope.fromTables.splice(i,1);
+                };
+            }
         };
     })
 ;
