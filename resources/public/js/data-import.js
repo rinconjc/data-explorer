c8f301e4b002d9d5df71f8bdcdcd8724c2999939
Julio <jcrinconc@gmail.com>
2015-08-04 07:25:20 +1000

    data import progress

--- resources/public/js/data-import.js
+++ resources/public/js/data-import.js
@@ -5,6 +5,7 @@ angular.module('data-import', ['ui.bootstrap', 'common-utils', 'common-widgets']
                 templateUrl:'tpls/data-import.html',
                 size:'lg',
                 controller:function($scope, $modalInstance){
+                    var FORMAT_REQUIRED_TYPES = [91,92,93,3,8,2,7];
                     $scope.model = {separator:'\t', hasHeader:true};
                     $scope.cancel = function(){
                         $modalInstance.dismiss();
@@ -21,13 +22,23 @@ angular.module('data-import', ['ui.bootstrap', 'common-utils', 'common-widgets']
                     ];
                     $scope.dest={};
                     function updateTables(){
-                        $scope.destParams[1].options = DataService.getTables($scope.dest.database);
+                        $scope.destParams[1].options = CommonUtils.toObject(DataService.getTables($scope.dest.database));
                     }
+                    function tableChanged(){
+                        $scope.tableMeta = DataService.getTableInfo($scope.dest.database, $scope.dest.table);
+                    }
+                    $scope.requiresFormat = function(col){
+                        return FORMAT_REQUIRED_TYPES.indexOf(col.data_type)>=0;
+                    };
                     $scope.destParams = [
                         {field:'database', label:'Database', type:'select', onchange:updateTables,
                          options:CommonUtils.toObject(DataService.getDatasources(), 'id', 'name')},
-                        {field:'table', label:'Table', type:'select', options:{}}
-                    ]
+                        {field:'table', label:'Table', type:'select', onchange:tableChanged, options:{}}
+                    ];
+                    $scope.doImport = function(){
+                        console.log('importing into...', $scope.dest);
+                                                
+                    };
                 }
             })
         });
