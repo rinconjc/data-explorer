30c92696429c3096c26202106536b0591767b527
Julio <jcrinconc@gmail.com>
2015-10-16 07:39:36 +1100

    sorting tables

--- src/cljs/dbquery/data_table.cljs
+++ src/cljs/dbquery/data_table.cljs
@@ -4,27 +4,40 @@
             [ajax.core :refer [GET POST]]))
 
 (defn data-table [data]
-  [:div.table-responsive
-   [:table.table {:class "table-hover"}
-    [:thead
-     [:tr
-      (for [c (@data "columns")]
-        [:th c [:i.fa {:class "fa-sort"}]])]]
-    [:tbody
-     (for [row (@data "rows")]
-       [:tr
-        (for [v row]
-          [:td v])])]
-    [:tfoot
-     [:tr
-      [:td {:colspan (-> @data "columns" count)}
-       [c/button "More"]]]]]])
+  (let [sort-state (atom [])
+        sort-icons (atom {})
+        roll-sort (fn[i]
+                    (let [next-icon
+                          (-> (case (@sort-icons i "fa-sort")
+                             "fa-sort" ["fa-sort-up" (swap! sort-state conj i)]
+                             "fa-sort-up" ["fa-sort-down" (swap! sort-state (partial replace {i (- i)}))]
+                             "fa-sort-down" ["fa-sort" (swap! sort-state (partial remove #(= (- i))))]) first)]
+                      (swap! sort-icons assoc i next-icon)))]
+    (fn [data]
+      [:div.table-responsive
+       {:style {:overflow-y "scroll" :height "100%"}}
+       [:table.table {:class "table-hover table-bordered summary"}
+        [:thead
+         [:tr [:th {:style {:width "1px"}}]
+          (doall (map-indexed
+            (fn[i c] ^{:key i}
+              [:th c [:a {:class "btn-link" :on-click #(roll-sort i)}
+                      [:i.fa {:class (@sort-icons i "fa-sort")}]]]) (@data "columns")))]]
+        [:tbody
+         (map-indexed
+          (fn [i row]
+            ^{:key i}[:tr [:td (inc i)]
+                      (map-indexed
+                       (fn[j v] ^{:key j}[:td v]) row)]) (@data "rows"))]
+        [:tfoot
+         [:tr [:td {:col-span (count (@data "columns"))}
+               [c/button "More"]]]]]])))
 
 (defn query-table [ds query]
-  (let [data (atom [])
+  (let [data (atom {})
         error (atom nil)]
     (POST (str "/ds/" (ds "id") "/exec-sql")
-          :params query
+          :params query :response-format :json :format :json
           :handler #(reset! data (% "data"))
           :error-handler #(reset! error %))
-    (fn[] [data-table data])))
+    (fn[ds query] [data-table data])))
