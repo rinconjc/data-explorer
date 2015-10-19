8d0de5409fde68c6e4c5a815a59ac4000c99206f
Julio <jcrinconc@gmail.com>
2015-10-20 07:22:54 +1100

    shortcut keys

--- src/cljs/dbquery/db_console.cljs
+++ src/cljs/dbquery/db_console.cljs
@@ -3,6 +3,7 @@
             [widgets.splitter :as st]
             [dbquery.data-table :refer [query-table]]
             [reagent.core :as r :refer [atom]]
+            [cljsjs.mousetrap]
             [ajax.core :refer [GET POST]]))
 
 
@@ -11,14 +12,20 @@
        :handler #(reset! resp-atom %)
        :error-handler #(reset! error-atom %)))
 
-(defn db-objects [db ops]
+(defn db-objects [db ops active?]
   (let [tables (atom [])
         error (atom nil)
         selected (atom nil)
+        search? (atom false)
         icons {"TABLE" "fa-table fa-fw"
                "VIEW" "fa-copy fa-fw"}
         _ (retrieve-db-objects db tables error)]
-    (fn [db ops]
+
+    (fn [db ops active?]
+      (when active?
+        (js/Mousetrap.bind "alt+d" #((:preview-table ops) (@selected "name")))
+        (js/Mousetrap.bind "/" #(swap! search? not))
+        (js/Mousetrap.bind "esc" #(reset! search? false)))
       [:div {:class "full-height panel panel-default"}
        [:div.panel-heading {:class "compact"}
         [c/button-group
@@ -26,10 +33,12 @@
                     {:class "glyphicon-refresh" :title "Refresh Objects"
                      :on-click #(retrieve-db-objects db tables error :refresh true)}]]
          [c/button {:on-click #((:preview-table ops) (@selected "name"))}
-          [:span.glyphicon
-           {:class "glyphicon-list-alt" :title "Preview Data"}]]
-         [c/button [:span.glyphicon {:class "glyphicon-info-sign" :title "Show metadata"}]]]]
-       [:div.panel-body
+          [:span.glyphicon.glyphicon-list-alt {:title "Preview Data"}]]
+         [c/button
+          [:span.glyphicon.glyphicon-info-sign {:title "Show metadata"}]]]]
+       [:div.panel-body {:style {:padding "4px 4px"}}
+        (if @search? (with-meta [:input.form-control {:placeholder "search..." :size 35 :style {:width "100%"}}]
+                       {:component-did-mount #(.focus (r/dom-node %))}))
         [:span @error]
         [:ul {:class "list-unstyled list" :style {:height "100%" :cursor "pointer"}}
          (doall (for [tb @tables]
@@ -38,19 +47,20 @@
                         :on-click #(reset! selected tb)}
                    [:i.fa {:class (icons (tb "type"))}] (tb "name")]))]]])))
 
-(defn db-console [db]
+(defn db-console [db active?]
   (let [active-tab (atom nil)
         data-tabs (atom [])
-        ops {:preview-table (fn[tbl]
-                              (when-not (some #(= tbl (:id %)) @data-tabs)
-                                (.log js/console "adding table " tbl)
-                                (swap! data-tabs conj {:id tbl :raw-sql (str "select * from " tbl)}))
-                              (reset! active-tab tbl))}]
-    (fn[db]
+        ops {:preview-table
+             (fn[tbl]
+               (when-not (some #(= tbl (:id %)) @data-tabs)
+                 (.log js/console "adding table " tbl)
+                 (swap! data-tabs conj {:id tbl :raw-sql (str "select * from " tbl)}))
+               (reset! active-tab tbl))}]
+    (fn[db active?]
       [st/horizontal-splitter {:split-at 240}
-       [db-objects db ops]
+       [db-objects db ops active?]
        [st/vertical-splitter {:split-at 200}
-        [:div "top pane, tabs count:" (count @data-tabs) ]
+        [:div "active?:" (if active? "active" "not-active")]
         [c/tabs {:activeKey @active-tab :on-select #(reset! active-tab %)
                  :class "small-tabs full-height"}
          (for [t @data-tabs]
