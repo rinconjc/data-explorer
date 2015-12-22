54990dbd1645552269b28e7b8f8ea9e0f6a98717
Julio <jcrinconc@gmail.com>
2015-12-23 07:32:15 +1100

    minor improvements

--- src/cljs/dbquery/data_import.cljs
+++ src/cljs/dbquery/data_import.cljs
@@ -17,13 +17,78 @@
 (defn format-required? [type]
   (#{91 92 93 3 8 2 7} type))
 
+(defn new-table-with-fields [import-form data dest-error]
+  (let [data-types (atom nil)]
+
+    (GET (str "/ds/" (:database @import-form) "/data-types") :response-format :json
+         :handler #(reset! data-types %) :error-handler #(reset! dest-error (error-text %)))
+
+    (fn [import-form data dest-error]
+      [:div.table-responsive
+       [:table.table-bordered.table-stripped.summary
+        [:thead [:tr [:th "#"] [:th "Dest. Column"] [:th "Data Type"] [:th "Size"]
+                 [:th "Source Column"] [:th "Format"] [:th "Sequential"]]]
+        [:tbody
+         (doall (map-indexed
+                 (fn [i col] ^{:key i}
+                   [:tr
+                    [:td [button
+                          {:on-click #(doto import-form
+                                        (swap! :columns (fn [form] (remove-nth (:columns form) i)))
+                                        (swap! :mappings (fn [form] (remove-nth (:mappings form) i))))
+                           :title "Remove" :bsStyle "primary"}
+                          [:i.fa.fa-minus]]]
+                    [:td [bare-input {:type "text" :model [import-form :columns i :column_name] :size 20}]]
+                    [:td [bare-input {:type "select" :model [import-form :columns i :type]
+                                      :options (for [t @data-types] [(t "data_type") (t "type_name")])}]]
+                    [:td (if (size-required? (get-in @import-form [:columns i :type]))
+                           [bare-input {:type "text" :model [import-form :columns i :size] :size 5}])]
+                    [:td [bare-input {:type "select" :model [import-form :mappings i :source]
+                                      :options (for [h (@data "header")] [h h])}]]
+                    [:td (if (format-required? (get-in @import-form [:columns i :type]))
+                           [bare-input {:type "text" :model [import-form :mappings i :format]}])]
+                    [:td]]) (@import-form :columns)))]
+        [:tfoot
+         [:tr [:td {:col-span 7}
+               [button {:on-click #(doto import-form
+                                     (swap! (fn [form] (assoc form :columns (conj (:columns form) {}))))
+                                     (swap! (fn [form] (assoc form :mappings (conj (:mappings form) {})))))
+                        :title "Add column" :bsStyle "default"}
+                [:i.fa.fa-plus]]]]]]])))
+
+(defn table-mappings [import-form data dest-error]
+  (let [headers (@data "header")]
+
+    (GET (str "/ds/" (:database @import-form) "/tables/" (:table @import-form))
+         :response-format :json
+         :handler #(swap! import-form assoc :mappings
+                          (into {} (for [c (% "columns") :let [{:strs[name data_type]} c]]
+                                     [name {:type data_type}])))
+         :error-handler #(reset! dest-error (error-text %)))
+
+    (fn [import-form data dest-error]
+      [:div.table-responsive
+       [:table.table-bordered.table-stripped.summary
+        [:thead
+         [:tr [:th "Dest. Column"] [:th "Source Column"] [:th "Format"] [:th "Sequential?"]]]
+        [:tbody
+         (for [[name {:keys[type]}] (@import-form :mappings)]
+           ^{:key name}
+           [:tr
+            [:td name]
+            [:td
+             [bare-input {:type "select" :model [import-form :mappings name :source]
+                          :options (for [h headers][h h])}]]
+            [:td
+             (if (format-required? type)
+               [bare-input {:type "text" :model [import-form :mappings name :format] :size 12}])]
+            [:td ]])]]])))
+
 (defn import-dest-form [data]
   (let [dest-error (atom nil)
         tables (atom nil)
-        columns (atom nil)
         data-sources (atom nil)
-        import-form (atom {:columns [] :mappings []})
-        data-types (atom nil)]
+        import-form (atom {:columns [] :mappings []})]
 
     (GET "/data-sources" :response-format :json
          :handler #(do (reset! data-sources %) (reset! dest-error nil))
@@ -33,14 +98,7 @@
                (fn [k r {old-db :database old-table :table} {new-db :database new-table :table}]
                  (when (not= old-db new-db)
                    (GET (str "/ds/" new-db "/tables") :response-format :json
-                        :handler #(reset! tables %) :error-handler #(reset! dest-error (error-text %)))
-                   (GET (str "/ds/" new-db "/data-types") :response-format :json
-                        :handler #(reset! data-types %) :error-handler #(reset! dest-error (error-text %))))
-                 (when (not= old-table new-table)
-                   (if (= new-table "_")
-                     (reset! columns nil)
-                     (GET (str "/ds/" new-db "/tables/" new-table) :response-format :json
-                          :handler #(reset! columns %) :error-handler #(reset! dest-error (error-text %)))))))
+                        :handler #(reset! tables %) :error-handler #(reset! dest-error (error-text %))))))
     (fn [data]
       [:div
        [:h4 "Preview Data:"]
@@ -64,37 +122,9 @@
         [:span.text-danger @dest-error]]
        [:div
         [:h4 "Field Mapping:"]
-        (if (= "_" (@import-form :table))
-          [:table.table-bordered.table-stripped.summary
-           [:thead [:tr [:th "#"] [:th "Dest. Column"] [:th "Data Type"] [:th "Size"]
-                    [:th "Source Column"] [:th "Format"] [:th "Sequential"]]]
-           [:tbody
-            (doall (map-indexed
-                    (fn [i col] ^{:key i}
-                      [:tr
-                       [:td [button
-                             {:on-click #(doto import-form
-                                           (swap! :columns (fn[form] (remove-nth (:columns form) i)))
-                                           (swap! :mappings (fn[form] (remove-nth (:mappings form) i))))
-                              :title "Remove" :bsStyle "primary"}
-                             [:i.fa.fa-minus]]]
-                       [:td [bare-input {:type "text" :model [import-form [:columns i :column_name]] :size 20}]]
-                       [:td [bare-input {:type "select" :model [import-form [:columns i :type]]
-                                         :options (for [t @data-types] [(t "data_type") (t "type_name")])}]]
-                       [:td (if (size-required? (get-in @import-form [:columns i :type]))
-                              [bare-input {:type "text" :model [import-form [:columns i :size]] :size 5}])]
-                       [:td [bare-input {:type "select" :model [import-form [:mappings i :source]]
-                                         :options (for [h (@data "header")] [h h])}]]
-                       [:td (if (format-required? (get-in @import-form [:columns i :type]))
-                              [bare-input {:type "text" :model [import-form [:mappings i :format]]}])]
-                       [:td]]) (@import-form :columns)))]
-           [:tfoot
-            [:tr [:td {:col-span 7}
-                  [button {:on-click #(doto import-form
-                                        (swap! (fn [form] (assoc form :columns (conj (:columns form) {}))))
-                                        (swap! (fn [form] (assoc form :mappings (conj (:mappings form) {})))))
-                           :title "Add column" :bsStyle "default"}
-                   [:i.fa.fa-plus]]]]]])]])))
+        (cond
+          (= "_" (@import-form :table)) [new-table-with-fields import-form data dest-error]
+          (some? (@import-form :table)) [table-mappings import-form data dest-error])]])))
 
 (defn import-data-tab []
   (let [upload-params (atom {:separator ","})
