baa069b6eb1eac3d3233843c8a78d291b93c45b5
julio <jcrinconc@gmail.com>
2016-07-08 11:30:20 +1000

    support binary field

--- src/clj/dbquery/core.clj
+++ src/clj/dbquery/core.clj
@@ -32,6 +32,7 @@
     (let [sdf (SimpleDateFormat. fmt)]
       (.writeString jsonGenerator (.format sdf d)))))
 
+(add-encoder (Class/forName "[B") #(.writeString %2 "<binary>"))
 (add-encoder java.util.Date (date-time-encoder "dd/MM/yyyy"))
 (add-encoder java.sql.Date (date-time-encoder "dd/MM/yyyy"))
 (add-encoder java.sql.Timestamp (date-time-encoder "dd/MM/yyyy HH:mm:ss"))
