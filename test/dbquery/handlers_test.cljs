532a597dbd62099f7d67e4717594801b4de83f1c
Julio Rincon <jcrinconc@gmail.com>
2020-06-23 11:46:28 +1000

    minor fixes

--- test/dbquery/handlers_test.cljs
+++ test/dbquery/handlers_test.cljs
@@ -1,9 +1,10 @@
 (ns dbquery.handlers-test
-  (:require [cljs.test :refer-macros [deftest is run-tests testing]]))
+  (:require [cljs.test :refer-macros [deftest is run-tests testing]]
+            [dbquery.sql-utils :as su ]))
 
 (deftest test-set-order
   (testing "set simple order"
-    (is (= [[1 :up]] (set-order [] 1 next-order)))))
+    (is (= [[1 :up]] (su/set-order [] 1 su/next-order)))))
 
 (cljs.test/report :end-run-tests)
 (cljs.test/run-tests)
